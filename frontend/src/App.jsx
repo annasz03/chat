4e7fbcf71a2155d7e8251b92a479bfe5fe81007b
Szabó Anna <szabo.anna.8@stud.u-szeged.hu>
2024-07-10 10:42:53 +0200

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,11 @@ function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Home />
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/signup' element={<SignUp />}/>
+      </Routes>
     </div>
   )
 }
