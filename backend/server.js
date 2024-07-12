3be0357509bf913e1954f07c065dca6d51df17d0
Szabó Anna <szabo.anna.8@stud.u-szeged.hu>
2024-07-12 18:46:51 +0200

    kesz

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const __ddirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,10 +20,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__ddirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req,res) => {
-    res.sendFile(path.join(__ddirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
 
 server.listen(PORT, () => {
